7786ae2f37b8a0d1eebbc4201066d2f7b6189e8d
Vinicius Santos <viniciushacrinha1@gmail.com>
2021-08-11 03:16:16 -0300

    Implementing the delete function

--- app/src/main/java/com/example/guests/service/repository/GuestRepository.kt
+++ app/src/main/java/com/example/guests/service/repository/GuestRepository.kt
@@ -55,9 +55,33 @@ class GuestRepository private constructor(context: Context) {
     // CRUD
 
 
-    fun update(guest: GuestModel) {
+    fun update(guest: GuestModel): Boolean {
+        return try {
+            val db = mGuestDataBaseHelper.writableDatabase
+
+            val contentValue = ContentValues()
+            contentValue.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            contentValue.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, guest.presence)
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+            val args = arrayOf(guest.id.toString())
+
+            db.update(DataBaseConstants.GUEST.TABLE_NAME, contentValue, selection,args)
+            true
+        } catch (e: Exception) {
+            false
+        }
     }
 
-    fun delete(guest: GuestModel) {
+    fun delete(id: Int): Boolean {
+        return try {
+            val db = mGuestDataBaseHelper.writableDatabase
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+            val args = arrayOf(id.toString())
+
+            db.delete(DataBaseConstants.GUEST.TABLE_NAME,selection,args)
+            true
+        } catch (e: Exception) {
+            false
+        }
     }
 }
